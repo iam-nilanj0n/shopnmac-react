c5775062ca031f1627239862ff2bb307a36218cb
iam-nilanj0n <98917336+iam-nilanj0n@users.noreply.github.com>
2023-05-10 17:43:54 +0530

    shop-n-mac

---
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HeaderComponent.css';
+
+export default function HeaderComponent() {
+  return (
+    <div className='HeaderComponent'>
+        <section>
+            <div className='logoAndOthersContainer'>
+            </div>
+        </section>
+    </div>
+  )
+}
