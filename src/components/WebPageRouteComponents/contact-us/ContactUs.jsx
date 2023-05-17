c3423be866189146936429f3c554e924772fc871
iam-nilanj0n <98917336+iam-nilanj0n@users.noreply.github.com>
2023-05-17 11:58:45 +0530

    Update ContactUs.jsx

--- src/components/WebPageRouteComponents/contact-us/ContactUs.jsx
+++ src/components/WebPageRouteComponents/contact-us/ContactUs.jsx
@@ -4,10 +4,42 @@ import HeaderComponent from '../../LayOutComponents/HeaderComponent/HeaderCompon
 import FooterComponent from '../../LayOutComponents/FooterComponent/FooterComponent';
 
 export default function ContactUs() {
+
+  const contactUsFormSubmitHandler = (event)=>{
+     event.preventDefault()
+  }
   return (
     <div className='ContactUs'>
         <HeaderComponent />
-        <h1>Contact-Us page is here</h1>
+        <main className='ContactUs-Main'>
+          <div className='ContactUs-Main-Left'>
+            <h1>Do you encounter any problems purchasing your goods?</h1>
+            <form>
+
+              <label>Email</label>
+              <input type='email'/>
+
+              <label>First Name</label>
+              <input type='text'/>
+
+              <label>Last Name</label>
+              <input type='text'/>
+
+              <label>Company Name</label>
+              <input type='text'/>
+
+              <label>Phone Number</label>
+              <input type='text'/>
+
+              <label>Message</label>
+              <textarea></textarea>
+
+              <button onClick={(e)=>contactUsFormSubmitHandler(e)} >Send Message</button>
+            </form>
+          </div>
+
+          <div className='ContactUs-Main-Right'></div>
+        </main>
         <FooterComponent />
     </div>
   )
